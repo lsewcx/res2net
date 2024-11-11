ec8762021bc1bc1052d708d0f8bab2fb387745b6
lsewcx <lse210085@163.com>
2024-11-11 19:44:23 +0800

    修改数据集配置文件

--- configs/res2net/res2net50-w14-s8_8xb32_in1k.py
+++ configs/res2net/res2net50-w14-s8_8xb32_in1k.py
@@ -2,34 +2,35 @@ _base_ = [
     # '../_base_/models/res2net50-w14-s8.py',
     # '../_base_/datasets/imagenet_bs32_pil_resize.py',
     # '../_base_/schedules/imagenet_bs256.py',
-    '../_base_/default_runtime.py'
+    "../_base_/default_runtime.py"
 ]
 
 model = dict(
-    type='ImageClassifier',
+    type="ImageClassifier",
     backbone=dict(
-        type='Res2Net',
+        type="Res2Net",
         depth=50,
         scales=8,
         base_width=14,
         deep_stem=False,
         avg_down=False,
     ),
-    neck=dict(type='GlobalAveragePooling'),
+    neck=dict(type="GlobalAveragePooling"),
     head=dict(
-        type='LinearClsHead',
+        type="LinearClsHead",
         num_classes=32,
         in_channels=2048,
-        loss=dict(type='CrossEntropyLoss', loss_weight=1.0),
+        loss=dict(type="CrossEntropyLoss", loss_weight=1.0),
         topk=(1, 5),
     ),
     init_cfg=dict(
-            type='Pretrained',
-            checkpoint='https://download.openmmlab.com/mmclassification/v0/res2net/res2net50-w14-s8_3rdparty_8xb32_in1k_20210927-bc967bf1.pth'),
+        type="Pretrained",
+        checkpoint="https://download.openmmlab.com/mmclassification/v0/res2net/res2net50-w14-s8_3rdparty_8xb32_in1k_20210927-bc967bf1.pth",
+    ),
 )
 
 # dataset settings
-dataset_type = 'ImageNet'
+dataset_type = "ImageNet"
 data_preprocessor = dict(
     num_classes=32,
     # RGB format normalization parameters
@@ -40,17 +41,17 @@ data_preprocessor = dict(
 )
 
 train_pipeline = [
-    dict(type='LoadImageFromFile'),
-    dict(type='RandomResizedCrop', scale=224, backend='pillow'),
-    dict(type='RandomFlip', prob=0.5, direction='horizontal'),
-    dict(type='PackInputs'),
+    dict(type="LoadImageFromFile"),
+    dict(type="RandomResizedCrop", scale=224, backend="pillow"),
+    dict(type="RandomFlip", prob=0.5, direction="horizontal"),
+    dict(type="PackInputs"),
 ]
 
 test_pipeline = [
-    dict(type='LoadImageFromFile'),
-    dict(type='ResizeEdge', scale=256, edge='short', backend='pillow'),
-    dict(type='CenterCrop', crop_size=224),
-    dict(type='PackInputs'),
+    dict(type="LoadImageFromFile"),
+    dict(type="ResizeEdge", scale=256, edge="short", backend="pillow"),
+    dict(type="CenterCrop", crop_size=224),
+    dict(type="PackInputs"),
 ]
 
 train_dataloader = dict(
@@ -58,13 +59,45 @@ train_dataloader = dict(
     num_workers=5,
     dataset=dict(
         type=dataset_type,
-        data_root='../../ImageNet1/train',
-        split='train',
-        classes=['JL-3', 'N1', 'N10', 'N13', 'N26', 'N47', 'N50', 'N53', 'N9', 'S11', 'S12', 'S18', 'S19', 'S2', 'S22',
-                 'S28', 'S31', 'S36', 'S43', 'S44', 'S48', 'S5', 'S52', 'S59', 'S62', 'S64', 'S73', 'S79', 'S90', 'S95',
-                 'SUN397', 'YDN6'],
-        pipeline=train_pipeline),
-    sampler=dict(type='DefaultSampler', shuffle=True),
+        data_root="/kaggle/working/res2net/ImageNet1/train",
+        split="train",
+        classes=[
+            "JL-3",
+            "N1",
+            "N10",
+            "N13",
+            "N26",
+            "N47",
+            "N50",
+            "N53",
+            "N9",
+            "S11",
+            "S12",
+            "S18",
+            "S19",
+            "S2",
+            "S22",
+            "S28",
+            "S31",
+            "S36",
+            "S43",
+            "S44",
+            "S48",
+            "S5",
+            "S52",
+            "S59",
+            "S62",
+            "S64",
+            "S73",
+            "S79",
+            "S90",
+            "S95",
+            "SUN397",
+            "YDN6",
+        ],
+        pipeline=train_pipeline,
+    ),
+    sampler=dict(type="DefaultSampler", shuffle=True),
 )
 
 val_dataloader = dict(
@@ -72,15 +105,47 @@ val_dataloader = dict(
     num_workers=5,
     dataset=dict(
         type=dataset_type,
-        data_root='../../ImageNet1/val',
-        split='val',
-        classes=['JL-3', 'N1', 'N10', 'N13', 'N26', 'N47', 'N50', 'N53', 'N9', 'S11', 'S12', 'S18', 'S19', 'S2', 'S22',
-                 'S28', 'S31', 'S36', 'S43', 'S44', 'S48', 'S5', 'S52', 'S59', 'S62', 'S64', 'S73', 'S79', 'S90', 'S95',
-                 'SUN397', 'YDN6'],
-        pipeline=test_pipeline),
-    sampler=dict(type='DefaultSampler', shuffle=False),
+        data_root="/kaggle/working/res2net/ImageNet1/val",
+        split="val",
+        classes=[
+            "JL-3",
+            "N1",
+            "N10",
+            "N13",
+            "N26",
+            "N47",
+            "N50",
+            "N53",
+            "N9",
+            "S11",
+            "S12",
+            "S18",
+            "S19",
+            "S2",
+            "S22",
+            "S28",
+            "S31",
+            "S36",
+            "S43",
+            "S44",
+            "S48",
+            "S5",
+            "S52",
+            "S59",
+            "S62",
+            "S64",
+            "S73",
+            "S79",
+            "S90",
+            "S95",
+            "SUN397",
+            "YDN6",
+        ],
+        pipeline=test_pipeline,
+    ),
+    sampler=dict(type="DefaultSampler", shuffle=False),
 )
-val_evaluator = dict(type='Accuracy', topk=(1, 5))
+val_evaluator = dict(type="Accuracy", topk=(1, 5))
 
 # If you want standard test, please manually configure the test dataset
 test_dataloader = val_dataloader
@@ -89,12 +154,14 @@ test_evaluator = val_evaluator
 # optimizer
 optim_wrapper = dict(
     # 使用 SGD 优化器来优化参数
-    optimizer=dict(type='SGD', lr=0.1, momentum=0.9, weight_decay=0.0001))
+    optimizer=dict(type="SGD", lr=0.1, momentum=0.9, weight_decay=0.0001)
+)
 
 # 学习率参数的调整策略
 # 'MultiStepLR' 表示使用多步策略来调度学习率（LR）。
 param_scheduler = dict(
-    type='MultiStepLR', by_epoch=True, milestones=[30, 60, 90], gamma=0.1)
+    type="MultiStepLR", by_epoch=True, milestones=[30, 60, 90], gamma=0.1
+)
 
 # 训练的配置，迭代 100 个 epoch，每一个训练 epoch 后都做验证集评估
 # 'by_epoch=True' 默认使用 `EpochBaseLoop`,  'by_epoch=False' 默认使用 `IterBaseLoop`
@@ -110,30 +177,24 @@ test_cfg = dict()
 auto_scale_lr = dict(base_batch_size=256)
 
 
-
 # defaults to use registries in mmpretrain
-default_scope = 'mmpretrain'
+default_scope = "mmpretrain"
 
 # configure default hooks
 default_hooks = dict(
     # record the time of every iteration.
-    timer=dict(type='IterTimerHook'),
-
+    timer=dict(type="IterTimerHook"),
     # print log every 100 iterations.
-    logger=dict(type='LoggerHook', interval=10),
-
+    logger=dict(type="LoggerHook", interval=10),
     # enable the parameter scheduler.
-    param_scheduler=dict(type='ParamSchedulerHook'),
-
+    param_scheduler=dict(type="ParamSchedulerHook"),
     # save checkpoint per epoch.
-    checkpoint=dict(type='CheckpointHook', interval=1),
+    checkpoint=dict(type="CheckpointHook", interval=1),
     # checkpoint=dict(type='CheckpointHook', interval=1, save_best="auto", max_keep_ckpt=3),
-
     # set sampler seed in distributed evrionment.
-    sampler_seed=dict(type='DistSamplerSeedHook'),
-
+    sampler_seed=dict(type="DistSamplerSeedHook"),
     # validation results visualization, set True to enable it.
-    visualization=dict(type='VisualizationHook', enable=False),
+    visualization=dict(type="VisualizationHook", enable=False),
 )
 
 
@@ -141,22 +202,20 @@ default_hooks = dict(
 env_cfg = dict(
     # whether to enable cudnn benchmark
     cudnn_benchmark=False,
-
     # set multi process parameters
-    mp_cfg=dict(mp_start_method='fork', opencv_num_threads=0),
-
+    mp_cfg=dict(mp_start_method="fork", opencv_num_threads=0),
     # set distributed parameters
-    dist_cfg=dict(backend='nccl'),
+    dist_cfg=dict(backend="nccl"),
 )
 
 # set visualizer
 # vis_backends = [dict(type='LocalVisBackend'),dict(type='TensorboardVisBackend')]
-vis_backends = [dict(type='LocalVisBackend')]
+vis_backends = [dict(type="LocalVisBackend")]
 
-visualizer = dict(type='UniversalVisualizer', vis_backends=vis_backends)
+visualizer = dict(type="UniversalVisualizer", vis_backends=vis_backends)
 
 # set log level
-log_level = 'INFO'
+log_level = "INFO"
 
 # load from which checkpoint
 # load_from ='E:/configs/_base_/pre_train/res2net50-w14-s8_3rdparty_8xb32_in1k_20210927-bc967bf1.pth'
